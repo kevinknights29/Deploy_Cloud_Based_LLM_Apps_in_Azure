230c5fcdf20a5cfe65f45a0f078291adf050a194
Kevin Knights <kevinknights29@hotmail.com>
2023-11-24 19:41:54 -0500

    added utils submodule

---
+++ src/utils/constants.py
@@ -0,0 +1,13 @@
+import os
+
+
+# Directories
+UTILS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PACKAGE = os.path.dirname(UTILS_FOLDER)
+ROOT = os.path.dirname(PROJECT_PACKAGE)
+
+# Files
+CONFIG_FILE = os.path.join(ROOT, "config.yaml")
+
+# Logging
+LOGGING_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
