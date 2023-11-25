cc5344879a6bcf7ed0a673e1dbd50a72a3d301d3
Kevin Knights <kevinknights29@hotmail.com>
2023-11-24 20:20:52 -0500

    WIP: adding social content generation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from src.utils import common
+from src.llm import openai
 
 APP_TITLE = common.config()["app"]["title"]
 APP_ONBOARDING = common.config()["app"]["onboarding"]
@@ -12,7 +13,9 @@ SUBMIT_BUTTON = common.config()["app"]["submit"]
 
 st.title(APP_TITLE)
 st.markdown("\n".join(APP_ONBOARDING))
-st.text_area(INPUT_INSTRUCTION)
-st.selectbox(SELECT_BOX_INSTRUCTION, SELECT_BOX_OPTIONS)
-st.text_area(OUTPUT_INSTRUCTION)
-st.button(SUBMIT_BUTTON)
+content = st.text_area(INPUT_INSTRUCTION)
+tone = st.selectbox(SELECT_BOX_INSTRUCTION, SELECT_BOX_OPTIONS)
+
+if st.button(SUBMIT_BUTTON):
+    post_content = openai.generate_post(content, tone)
+    st.text_area(OUTPUT_INSTRUCTION, value=post_content, height=500)
