cd1dde0c7cd8d12cb818a3d8a6504a68390cedc4
Kevin Knights <kevinknights29@hotmail.com>
2023-11-25 12:27:16 +0000

    added create_logger function

--- src/utils/common.py
+++ src/utils/common.py
@@ -32,3 +32,31 @@ def config() -> dict:
             conf = yaml.load(cfg, Loader=SafeLoader)
         logger.log(logging.INFO, "Config loaded")
     return conf
+
+
+def create_logger(logger_name, level=logging.INFO, fmt=LOGGING_FORMAT):
+    """
+    Creates and configures a logger object.
+
+    Args:
+        logger_name (str): The name of the logger.
+        level (int): The log level to use (default: logging.INFO).
+
+    Returns:
+        logging.Logger: The configured logger object.
+    """
+    if logger_name in create_logger.loggers:
+        return create_logger.loggers[logger_name]
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(fmt)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    create_logger.loggers[logger_name] = logger
+    return logger
+
+
+create_logger.loggers = {}
