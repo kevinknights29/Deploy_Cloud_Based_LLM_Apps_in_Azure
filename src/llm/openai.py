d5061c59c838b1e2cd88ccb6e6760cfcdaa04c1e
Kevin Knights <kevinknights29@hotmail.com>
2023-11-24 20:20:18 -0500

    added llm submodule

---
+++ src/llm/openai.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from openai import OpenAI
+
+CLIENT = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+SYSTEM_PROMPT = "You are a social media content writer. Your tasks is to be helpful, creative, clever, and engaging."
+USER_PROMPT = "Based on the following content and tone, write a social media post that will get the most engagement."
+
+
+def generate_post(
+    content: str, tone: str, client: OpenAI = CLIENT, system_prompt: str = SYSTEM_PROMPT, user_prompt: str = USER_PROMPT
+) -> str:
+    """
+    Generates a post using OpenAI's GPT-3.5-turbo model.
+
+    Args:
+        content (str): The content of the post.
+        tone (str): The desired tone of the post.
+        client (OpenAI, optional): The OpenAI client. Defaults to CLIENT.
+        system_prompt (str, optional): The system prompt for the conversation. Defaults to SYSTEM_PROMPT.
+        user_prompt (str, optional): The user prompt for the conversation. Defaults to USER_PROMPT.
+
+    Returns:
+        str: The generated post.
+
+    """
+    # Add the content and tone to the user prompt
+    user_prompt += f"\n\nContent: ```{content}```\n\nTone: ```{tone}```"
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
+    return response.choices[0].message.content
