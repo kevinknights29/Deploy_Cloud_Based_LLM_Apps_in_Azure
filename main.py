6ac2cf99394b2ef2c01d113aee231375394ef7b9
Kevin Knights <kevinknights29@hotmail.com>
2023-11-25 12:27:29 +0000

    added file upload support

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
+from io import StringIO
+
 import streamlit as st
+import PyPDF2
 
 from src.utils import common
 from src.llm import openai
 
+logger = common.create_logger(__name__)
+
 APP_TITLE = common.config()["app"]["title"]
 APP_ONBOARDING = common.config()["app"]["onboarding"]
 INPUT_INSTRUCTION = common.config()["app"]["input"]
+UPLOAD_INSTRUCTION = common.config()["app"]["upload"]
+ALLOWED_EXTENSIONS = common.config()["settings"]["extensions"]
 SELECT_BOX_INSTRUCTION = common.config()["app"]["select_box"]["instruction"]
 SELECT_BOX_OPTIONS = common.config()["app"]["select_box"]["options"]
 OUTPUT_INSTRUCTION = common.config()["app"]["output"]
 SUBMIT_BUTTON = common.config()["app"]["submit"]
 
+# Set the page title and onboarding text
 st.title(APP_TITLE)
 st.markdown("\n".join(APP_ONBOARDING))
-content = st.text_area(INPUT_INSTRUCTION)
+
+# Select the content and tone
+content = None
+uploaded_file = st.file_uploader(UPLOAD_INSTRUCTION, type=ALLOWED_EXTENSIONS)
+if uploaded_file is not None:
+    if uploaded_file.type == "application/pdf":
+        # Read the PDF file
+        reader = PyPDF2.PdfReader(uploaded_file)
+        # Extract the content
+        page_content = []
+        for page in reader.pages:
+            if page.extract_text() is not None:
+                clean_page = page.extract_text().strip().replace("\n", " ")
+                page_content.append(clean_page)
+        content = "\n".join(page_content)
+    else:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        # To read file as string:
+        content = stringio.read()
+
+content = st.text_area(INPUT_INSTRUCTION, value=content)
 tone = st.selectbox(SELECT_BOX_INSTRUCTION, SELECT_BOX_OPTIONS)
 
+# Generate the post
 if st.button(SUBMIT_BUTTON):
     post_content = openai.generate_post(content, tone)
     st.text_area(OUTPUT_INSTRUCTION, value=post_content, height=500)
